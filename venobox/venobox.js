cb47baf525a10527edc7c8552a361ee9f245698b
Giel Berkers <info@gielberkers.com>
2014-09-17 13:31:21 +0200

    Check if transition is on the overlay

--- venobox/venobox.js
+++ venobox/venobox.js
@@ -104,6 +104,7 @@
                       });
                     } else {
                       overlay.bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
+                        if(overlay[0] != e.target) { return; }
                         overlay.css({
                           'min-height': $(window).outerHeight(),
                           height : 'auto'
