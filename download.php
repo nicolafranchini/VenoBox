0cc1fe79becec63e1d248432bf1f5bb9df5c609c
veno 15 <veno@MacBook-Pro-di-veno.local>
2016-02-25 21:32:48 +0000

    update repository

--- download.php
+++
@@ -1,7 +0,0 @@
-<?php if(isset($_GET['file'])){
-	$file = $_GET['file'];
-	$hit_count = @file_get_contents('count.txt');
-	$hit_count++;
-	@file_put_contents('count.txt', $hit_count);
-	header('Location: http://lab.veno.it/venobox/'.$file); // redirect to the real file to be downloaded
-} ?>
