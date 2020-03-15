471b28764b3568bc9218039a97f779294b528516
Nicola Franchini <info@veno.es>
2020-03-15 20:58:25 +0000

    Version 1.8.7 - Update: Percentual window padding - Minified CSS

--- venobox/venobox.js
+++ venobox/venobox.js
@@ -1,12 +1,12 @@
 /*
  * VenoBox - jQuery Plugin
- * version: 1.8.6
+ * version: 1.8.7
  * @requires jQuery >= 1.7.0
  *
  * Examples at http://veno.es/venobox/
  * License: MIT License
  * License URI: https://github.com/nicolafranchini/VenoBox/blob/master/LICENSE
- * Copyright 2013-2019 Nicola Franchini - @nicolafranchini
+ * Copyright 2013-2020 Nicola Franchini - @nicolafranchini
  *
  */
 
