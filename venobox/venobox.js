c23fc06ff950b08109295c3c4568d2269a021737
Philippe MALADJIAN <pmaladjian@localhost.localdomain>
2020-06-04 09:48:05 +0200

    Add enable/disable swipeNav

--- venobox/venobox.js
+++ venobox/venobox.js
@@ -18,7 +18,7 @@
         framewidth, frameheight, gallItems, infinigall, items, keyNavigationDisabled, margine, numeratio,
         overlayColor, overlay, title, thisgall, thenext, theprev, nextok, prevok, preloader, $preloader, navigation,
         obj, gallIndex, startouch, vbheader, images, startY, startX, endY, endX, diff, diffX, diffY, threshold,
-        share, sharelinks, vbfooter, sharepos, arrowNav, keyNav;
+        share, sharelinks, vbfooter, sharepos, arrowNav, keyNav, swipeNav;
 
     var pinIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.372-12 12 0 5.084 3.163 9.426 7.627 11.174-.105-.949-.2-2.405.042-3.441.218-.937 1.407-5.965 1.407-5.965s-.359-.719-.359-1.782c0-1.668.967-2.914 2.171-2.914 1.023 0 1.518.769 1.518 1.69 0 1.029-.655 2.568-.994 3.995-.283 1.194.599 2.169 1.777 2.169 2.133 0 3.772-2.249 3.772-5.495 0-2.873-2.064-4.882-5.012-4.882-3.414 0-5.418 2.561-5.418 5.207 0 1.031.397 2.138.893 2.738.098.119.112.224.083.345l-.333 1.36c-.053.22-.174.267-.402.161-1.499-.698-2.436-2.889-2.436-4.649 0-3.785 2.75-7.262 7.929-7.262 4.163 0 7.398 2.967 7.398 6.931 0 4.136-2.607 7.464-6.227 7.464-1.216 0-2.359-.631-2.75-1.378l-.748 2.853c-.271 1.043-1.002 2.35-1.492 3.146 1.124.347 2.317.535 3.554.535 6.627 0 12-5.373 12-12 0-6.628-5.373-12-12-12z" fill-rule="evenodd" clip-rule="evenodd"/></svg>';
     var fbIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm3 8h-1.35c-.538 0-.65.221-.65.778v1.222h2l-.209 2h-1.791v7h-3v-7h-2v-2h2v-2.308c0-1.769.931-2.692 3.029-2.692h1.971v3z"/></svg>';
@@ -55,6 +55,7 @@
                 overlayColor : 'rgba(23,23,23,0.85)',
                 spinner : 'double-bounce', // available: 'rotating-plane' | 'double-bounce' | 'wave' | 'wandering-cubes' | 'spinner-pulse' | 'chasing-dots' | 'three-bounce' | 'circle' | 'cube-grid' | 'fading-circle' | 'folding-cube'
                 spinColor : '#d2d2d2',
+                swipeNav: true,
                 titleattr: 'title', // specific attribute to get a title (e.g. [data-title]) - thanx @mendezcode
                 titleBackground: '#161617',
                 titleColor: '#d2d2d2',
@@ -420,10 +421,12 @@
                       prevok = false;
                     }
                     // activate swipe
-                    if (prevok === true || nextok === true) {
-                      content.on(TouchMouseEvent.DOWN, onDownEvent);
-                      content.on(TouchMouseEvent.MOVE, onMoveEvent);
-                      content.on(TouchMouseEvent.UP, onUpEvent);
+                    if (option.swipeNav == true) {
+                      if (prevok === true || nextok === true) {
+                        content.on(TouchMouseEvent.DOWN, onDownEvent);
+                        content.on(TouchMouseEvent.MOVE, onMoveEvent);
+                        content.on(TouchMouseEvent.UP, onUpEvent);
+                      }
                     }
                 }
 
